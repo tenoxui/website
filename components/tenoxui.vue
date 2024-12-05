f5b2642ed8dde6b4f8dd08d0ee0afbb17c159647
nousantx <nousantx@gmail.com>
2024-12-05 13:15:09 +0700

    feat: add tenoxui components

---
+++ components/tenoxui.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from 'vue'
+import { MakeTenoxUI } from '@tenoxui/core/full'
+
+const props = defineProps({
+  code: {
+    type: String,
+    required: true
+  }
+})
+
+onMounted(() => {
+  document.querySelectorAll('[data-tenox] *').forEach(element => {
+    new MakeTenoxUI({ element }).useDOM()
+  })
+})
+</script>
+
+<template>
+  <article data-tenox>
+    <div v-html="code"/>
+  </article>
+</template>
