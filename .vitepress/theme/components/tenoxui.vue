484a098be2486ac497aefafbe6322c9ca6ff34fa
nousantx <nousantx@gmail.com>
2024-12-05 23:07:52 +0700

    feat(components): moved tenoxui components

---
+++ .vitepress/theme/components/tenoxui.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from 'vue'
+import { MakeTenoxUI } from '@tenoxui/core/full'
+import config from '../../../tenoxui.config'
+
+// Define props
+const props = defineProps({
+  code: {
+    type: String,
+    required: true
+  }
+})
+
+onMounted(() => {
+  document.querySelectorAll('[data-tenox] *').forEach((element) => {
+    new MakeTenoxUI({ element, ...config }).useDOM()
+  })
+})
+</script>
+
+<template>
+  <article data-tenox>
+    <div class="p-1rem grid [--g1,--g2,bc]-$vp-c-gray-2 bw-1px bs-solid br-8px center h-mn-10rem">
+      <div v-html="code" />
+    </div>
+  </article>
+</template>
