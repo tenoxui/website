4ae31736441e8ca9f882a3601c94a46e6fa88459
nousantx <nousantx@gmail.com>
2024-12-07 17:47:38 +0700

    chore: add new links, edit link, and favicon

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,17 +3,24 @@ import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  markdown: {
-    codeTransformers: [transformerTwoslash()]
-  },
-  srcDir: 'pages',
   title: 'TenoxUI',
   titleTemplate: 'TenoxUI Docs',
   description: 'TenoxUI Documentation Site',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { property: 'og:image', content: '/tenoxui_social_card.jpg' }],
+    ['meta', { name: 'twitter:card', content: 'TenoxUI Documentation Site' }],
+    ['meta', { name: 'twitter:image', content: '/tenoxui_social_card.jpg' }]
+  ],
+
+  srcDir: 'pages',
+  markdown: {
+    codeTransformers: [transformerTwoslash()]
+  },
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Docs', link: '/docs/getting-started' }
+      { text: 'Docs', link: '/docs/intro/about' }
     ],
     sidebar: [
       {
@@ -26,22 +33,38 @@ export default defineConfig({
       },
       {
         text: 'Core Concept',
-        collapsed: false,
+        collapsed: true,
         items: [
           { text: 'The Basics', link: '/docs/core/basic' },
-          { text: 'Type & Property', link: '/docs/core/type-and-property' },
-          { text: 'Defined Value', link: '/docs/core/values' },
-          { text: 'Classes', link: '/docs/core/classes' },
-          { text: 'Aliases', link: '/docs/core/aliases' },
+          { text: 'Writing Values', link: '/docs/core/available-values' },
           { text: 'Pseudo Classes', link: '/docs/core/pseudo-class' },
           { text: 'Responsive Design', link: '/docs/core/breakpoint-and-responsive' },
           { text: 'Attributify Mode', link: '/docs/core/attributify' }
         ]
+      },
+      {
+        text: 'Config',
+        collapsed: true,
+        items: [
+          { text: 'Configuration Options', link: '/docs/config/options' },
+          { text: 'Type Shorthand', link: '/docs/config/properties' },
+          { text: 'Value Alias', link: '/docs/config/values' },
+          { text: 'Creating Utility', link: '/docs/config/classes' },
+          { text: 'Class Name Alias', link: '/docs/config/aliases' },
+          { text: 'Breakpoint', link: '/docs/config/breakpoints' }
+        ]
+      },
+      {
+        text: 'Examples',
+        collapsed: true,
+        items: [{ text: 'DOM Manipulation', link: '/docs/examples/dom-manipulation' }]
       }
     ],
     socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    lastUpdated: true,
     editLink: {
-      pattern: 'https://github.com/tenoxui/website/edit/main/pages/:path'
+      pattern: 'https://github.com/tenoxui/website/edit/main/pages/:path',
+      text: 'Edit this page on GitHub'
     },
     footer: {
       message: 'Released under the MIT License.',
